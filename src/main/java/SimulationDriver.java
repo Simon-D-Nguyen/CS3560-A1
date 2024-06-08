f06716c017f1939e104d22e1e1e4ba1a21097e1f
Simon Nguyen <simonnguyen99@gmail.com>
2024-06-07 23:57:55 -0700

    Finished Multiple choice addition to of SimulationDriver

--- src/main/java/SimulationDriver.java
+++ src/main/java/SimulationDriver.java
@@ -4,7 +4,11 @@ public class SimulationDriver {
 
     public static void main(String[] args) {
 
-        Random random = new Random();
+        System.out.println("CS3560 - Assignment 1");
+        System.out.println("Author: Simon Nguyen");
+
+
+        Random random = new Random(); //Add for the random number generator
 
         // Create Question
         System.out.println("--- Single-Choice Question ---");
@@ -18,23 +22,23 @@ public class SimulationDriver {
         iVote.setQuestion(q1);
 
 
-        //Create Student IDs
+        //Create Student IDs (randomly)
         Student[] students = new Student[20];
-        for(int i = 0;  i < students.length; i++) {
+        for (int i = 0; i < students.length; i++) {
             students[i] = new Student(random.nextInt(1000));
         }
 
-        //Create Student Answers
+        //Create Student Answers (randomly)
         List<Character> options = iVote.getOptions().stream().toList();
         Character[] studentAnswers = new Character[students.length];
 
-        for(int i = 0;  i < studentAnswers.length; i++) {
+        for (int i = 0; i < studentAnswers.length; i++) {
             int choice = random.nextInt(options.size());
             studentAnswers[i] = options.get(choice);
         }
 
         //Submit answers
-        for (int i = 0;  i < studentAnswers.length; i++) {
+        for (int i = 0; i < studentAnswers.length; i++) {
             iVote.submitAnswer(students[i], new HashSet<>(Arrays.asList(studentAnswers[i])));
         }
 
@@ -45,13 +49,49 @@ public class SimulationDriver {
 
 
 
-        // Create Question
-        System.out.println("--- Single-Choice Question ---");
-        Question q2 = new SingleChoiceQuestion("Water is wet?");
-        q2.addOption('1', "True");
-        q2.addOption('2', "False");
 
+
+
+
+        // Create Question
+        System.out.println("--- Multiple-Choice Question ---");
+        Question q2 = new MultipleChoiceQuestion("Which is best?");
+        q2.addOption('A', "White Bread");
+        q2.addOption('B', "Sourdough");
+        q2.addOption('C', "Whole Wheat");
+        q2.addOption('D', "Multi-grain");
+
+
+
+        //Configure Voting Service
         iVote.setQuestion(q2);
 
+
+        // Using same students as above ...
+        //Create Student answers
+        List<Character> multipleChoiceOptions = iVote.getOptions().stream().toList();
+        List<Set<Character>> studentMultipleChoiceAnswers = new ArrayList<>(students.length);
+
+        for (int k = 0; k < students.length; k++) {
+            int numberOfAnswers = random.nextInt(multipleChoiceOptions.size() - 1) + 1;
+            Set<Character> studentAnswer = new HashSet<>();
+
+            for (int j = 0; j <= numberOfAnswers; j++) {
+                int randomOptionIndex = random.nextInt(multipleChoiceOptions.size());
+
+                studentAnswer.add(multipleChoiceOptions.get(randomOptionIndex));
+            }
+
+            studentMultipleChoiceAnswers.add(k, studentAnswer);
+        }
+
+        //Submit answers
+        for (int i = 0; i < studentAnswers.length; i++) {
+            iVote.submitAnswer(students[i], studentMultipleChoiceAnswers.get(i));
+        }
+
+        //Voting service output
+        iVote.outputStatistics();
+        iVote.outputStudentAnswers();
     }
 }
