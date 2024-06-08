ee96761b173e9cc0330ae8f28a83815055109b56
Simon Nguyen <simonnguyen99@gmail.com>
2024-06-07 22:39:43 -0700

    Edited Question to have answer checking as part of its functionality

--- src/main/java/Question.java
+++ src/main/java/Question.java
@@ -1,9 +1,37 @@
 import java.util.Collection;
+import java.util.Set;
 
+/**
+ * Holds the methods for any Question being
+ * read by VotingService
+ *
+ * Utilized an Interface to code by contract
+ * and to possibly expand implementation
+ */
 public interface Question {
 
+    /**
+     * Returns the question being asked.
+     * @return the String of the question
+     */
     public String getQuestion();
+
+    /**
+     * Adds an answer option to the question
+     * @param option The Character that defines the option
+     * @param description The description of the answer
+     */
     public void addOption(Character option, String description);
+
+    /**
+     * Returns a collection of the options characters
+     * @return a collection of characters representing options
+     */
     public Collection<Character> getOptions();
-    public boolean isMultiAnswer();
+
+    /**
+     * Describes if an answer-set is valid
+     * @return a boolean value representing if the answer is valid
+     */
+    public boolean isValidAnswer(Set<Character> answers);
 }
